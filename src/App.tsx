87ce415c5029d740b56f6ee661ac0bf804230397
SteamyCU <124738360+SteamyCU@users.noreply.github.com>
2026-08-19 20:46:45 +0200

    Add WhatsAppFloating component to App

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@
 import { Routes, Route } from "react-router-dom";
 import { Home } from "./components/Home";
 import { Contact } from "./components/Contact";
+import { WhatsAppFloating } from "./components/WhatsAppFloating";
 import { ReactLenis } from 'lenis/react';
 
 export default function App() {
@@ -15,6 +16,7 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/contacto" element={<Contact />} />
       </Routes>
+      <WhatsAppFloating />
     </ReactLenis>
   );
 }
