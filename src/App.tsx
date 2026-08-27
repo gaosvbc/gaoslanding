3bd1ea8dc6afe291f09aa02130229f5d9aa781c4
gaosvbc <gaosvbc@gmail.com>
2026-08-27 19:44:07 +0200

    Add StudioLanding route to App component

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { Routes, Route } from "react-router-dom";
 import { Home } from "./components/Home";
 import { Contact } from "./components/Contact";
 import { ReformasLanding } from "./components/ReformasLanding";
+import { StudioLanding } from "./components/StudioLanding";
 import AvisoLegal from "./components/AvisoLegal";
 import PoliticaPrivacidad from "./components/PoliticaPrivacidad";
 import PoliticaCookies from "./components/PoliticaCookies";
@@ -19,6 +20,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/reformas" element={<ReformasLanding />} />
+        <Route path="/studio" element={<StudioLanding />} />
         <Route path="/contacto" element={<Contact />} />
         <Route path="/aviso-legal" element={<AvisoLegal />} />
         <Route path="/politica-de-privacidad" element={<PoliticaPrivacidad />} />
